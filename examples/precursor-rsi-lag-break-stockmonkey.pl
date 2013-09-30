c0f0ea9fea892ae0a8f9de26d86da83de686c40d
Paul Miller <jettero@corky.vhb>
2013-09-30 06:52:52 -0400

    this is getting closer... needs more work

--- examples/precursor-rsi-lag-break-stockmonkey.pl
+++ examples/precursor-rsi-lag-break-stockmonkey.pl
@@ -10,16 +10,6 @@ use Math::Business::LaguerreFilter;
 use Data::Dump qw(dump);
 use GD::Graph::mixed;
 use List::Util qw(min max);
-use constant {
-    DATE   => 0,
-    CLOSE  => 1,
-    RSI    => 2,
-    LAG4   => 3,
-    LAG8   => 4,
-    EVENT  => 5,
-    BUY_P  => 6,
-    SELL_P => 7,
-};
 
 my $ticker = shift || "JPM";
 my $slurpp = "10 years"; # data we want to fetch
@@ -34,29 +24,33 @@ sub scan_for_events {
     my $last_row = $quotes->[0];
 
     for my $row ( @$quotes[1..$#$quotes] ) {
-        if( $last_row->[RSI] < 70 and $row->[RSI] >= 70 ) {
-            $row->[EVENT] = "OVERBOUGHT-1";
+        if( $last_row->{rsi} < 70 and $row->{rsi} >= 70 ) {
+            $row->{event} = "OVERBOUGHT-1";
         }
 
-        if( $last_row->[RSI] > 30 and $row->[RSI] <= 30 ) {
-            $row->[EVENT] = "OVERSOLD-1";
+        if( $last_row->{rsi} > 30 and $row->{rsi} <= 30 ) {
+            $row->{event} = "OVERSOLD-1";
         }
 
-        if( not exists $row->[EVENT] and exists $last_row->[EVENT] ) {
-            my ($event, $age) = $last_row->[EVENT] =~ m/(\w+)-(\d+)/;
-            $row->[EVENT] = "$event-" . ($age+1);
+        if( not exists $row->{event} and exists $last_row->{event} ) {
+            my ($event, $age) = $last_row->{event} =~ m/(\w+)-(\d+)/;
+            $row->{event} = "$event-" . ($age+1);
         }
 
-        if( exists $row->[EVENT] and my ($event, $age) = $row->[EVENT] =~ m/(\w+)-(\d+)/ ) {
-            if( $event eq "OVERBOUGHT" and $last_row->[RSI] > 60 and $row->[RSI] <= 60 ) {
-                $row->[EVENT] = "DOWNBREAK-1";
+        if( exists $row->{event} and my ($event, $age) = $row->{event} =~ m/(\w+)-(\d+)/ ) {
+            if( $event eq "OVERBOUGHT" and $last_row->{rsi} > 60 and $row->{rsi} <= 60 ) {
+                delete $row->{event};
+                if( $row->{lag4} < $row->{lag8} ) {
+                    $row->{EVENT} = "SELL";
+                }
             }
 
-            if( $event eq "OVERSOLD" and $last_row->[RSI] < 40 and $row->[RSI] >= 40 ) {
-                $row->[EVENT] = "UPBREAK-1";
+            if( $event eq "OVERSOLD" and $last_row->{rsi} < 40 and $row->{rsi} >= 40 ) {
+                delete $row->{event};
+                if( $row->{lag4} > $row->{lag8} ) {
+                    $row->{EVENT} = "BUY";
+                }
             }
-
-            if( $event eq "DOWNBREAK" and ????
         }
 
         $last_row = $row;
@@ -72,7 +66,7 @@ sub find_quotes_for {
 
     my $tick = uc(shift || "MSFT");
     my $time = lc(shift || "6 months");
-    my $fnam = "/tmp/$tick-$time.dat";
+    my $fnam = "/tmp/p2-$tick-$time.dat";
 
     my $res = eval { retrieve($fnam) };
     return $res if $res;
@@ -88,19 +82,20 @@ sub find_quotes_for {
     for my $row ($q->quotes) {
         my ($symbol, $date, $open, $high, $low, $close, $volume) = @$row;
 
-        my $row = [ $date, $close ];
-
         $rsi->insert( $close );
-        $row->[RSI] = $rsi->query;
-
         $lag4->insert( $close );
-        $row->[LAG4] = $lag4->query;
-
         $lag8->insert( $close );
-        $row->[LAG8] = $lag8->query;
+
+        my $row = {
+            date  => $date,
+            close => $close,
+            rsi   => $rsi->query,
+            lag4  => $lag4->query,
+            lag8  => $lag8->query,
+        };
 
         # only insert rows that are all defined
-        push @todump, $row unless grep {not defined} @$row;
+        push @todump, $row unless grep {not defined} values %$row;
     }
 
     store(\@todump => $fnam);
