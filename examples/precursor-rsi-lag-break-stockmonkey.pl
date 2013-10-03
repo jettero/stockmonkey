4c53523bc02f3f8aa96d131e4ffbc426b54439a7
Paul Miller <jettero@corky.vhb>
2013-10-03 13:32:24 -0400

    not using this

--- examples/precursor-rsi-lag-break-stockmonkey.pl
+++ examples/precursor-rsi-lag-break-stockmonkey.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use Finance::QuoteHist;
 use Storable qw(store retrieve);
-use Algorithm::NaiveBayes;
 use Math::Business::RSI;
 use Math::Business::LaguerreFilter;
 use Data::Dump qw(dump);
